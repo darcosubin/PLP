522ed15150fc78a8379fb1a2ac7db26d03859081
Darco Subin <darco.subin@3pillar.corp@tim-lap-085.3pillar.corp>
2019-07-24 15:38:52 +0300

    ex1.py

--- ex1.py
+++ ex1.py
@@ -11,6 +11,35 @@ class TestFlatten(unittest.TestCase):
             )
 
 def flatten(list_a,list_b,max_depth):
-    pass
+    c=conc(list_a,list_b)
+    elist=[]
+    for i in list_a:
+        if type(i)==list:
+            newI=flatten(i,[],max_depth)
+            if newI is not None:
+                for j in newI:
+                    elist.append(j)
+            else:
+                elist.append(i)
+    return elist
+
+
+def conc(list_a,list_b):
+
+    flattened=[]
+    for i in [list_a,list_b]:
+        for x in i:
+            flattened.append(x)
+    return flattened
+
+
+
 if __name__ == "__main__":
-    unittest.main()
+    print conc([1,2,3,[4,5]],['a','b','c','d',['e']])
+    a=[1,2,7,3,[4,5]] 
+    b=[1,2,[3,[4,5]]]
+    c=2
+    print flatten(a,b,c)
+    #unittest.main()
+
+
