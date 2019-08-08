630c8ea57c5d7061f19f5022b97c5609ab4f7ce8
Darco Subin <darco.subin@3pillar.corp@tim-lap-085.3pillar.corp>
2019-08-08 14:47:44 +0300

    ex2p.y

--- ex2.py
+++ ex2.py
@@ -1,21 +1,38 @@
 '''Merging two dictionaries'''
-
 def merge(first_dict, second_dict, new_dict):
     """This will merge the two dictionaries"""
-    for key in first_dict.keys():
-        if second_dict.get(key) is None:
-            print "Not found:",key
-            new_dict[key]=first_dict[key]
-        elif not isinstance(first_dict[key], type(second_dict[key])):
-            new_dict[key] = (first_dict[key], second_dict[key])
-        elif isinstance(first_dict[key], list):
-            new_dict[key] = conc(first_dict[key], second_dict[key])
-        elif isinstance(first_dict[key], set):
-            new_dict[key] = first_dict[key] | second_dict[key]
-        elif isinstance(first_dict[key], dict):
-            new_dict[key] = merge(first_dict[key], second_dict[key], {})
+    for key1,key2 in zip(first_dict.keys(),second_dict.keys()):
+        if second_dict.get(key1) is None:
+            print "Not found:",key1, key2
+            loop(second_dict,first_dict,new_dict)
+            loop(first_dict,second_dict,new_dict)
+        elif not isinstance(first_dict[key1], type(second_dict[key2])):
+            new_dict[key1] = (first_dict[key1], second_dict[key2])
+        elif isinstance(first_dict[key1], list):
+            new_dict[key1] = conc(first_dict[key1], second_dict[key2])
+        elif isinstance(first_dict[key1], set):
+            new_dict[key1] = first_dict[key1] | second_dict[key2]
+        elif isinstance(first_dict[key1], dict):
+            new_dict[key1] = merge(first_dict[key1], second_dict[key2], {})
         else:
-            new_dict[key] = first_dict[key] + second_dict[key]
+            new_dict[key1] = first_dict[key1] + second_dict[key2]
+    return new_dict
+
+def loop(first_dict, second_dict, new_dict):
+    for key1,key2 in zip(first_dict.keys(),second_dict.keys()):
+        if first_dict.get(key1) is None:
+            new_dict[key2] = first_dict[key1]
+            new_dict[key1] = first_dict[key2]
+        elif not isinstance(first_dict[key1], type(second_dict[key2])):
+            new_dict[key1] = (first_dict[key1], second_dict[key2])
+        elif isinstance(first_dict[key1], list):
+            new_dict[key1] = conc(first_dict[key1], second_dict[key2])
+        elif isinstance(first_dict[key1], set):
+            new_dict[key1] = first_dict[key1] | second_dict[key2]
+        elif isinstance(first_dict[key1], dict):
+            new_dict[key1] = merge(first_dict[key1], second_dict[key2], {})
+        else:
+            new_dict[key1] = first_dict[key1] + second_dict[key2]
     return new_dict
 
 
@@ -31,8 +48,11 @@ def conc(list_a, list_b):
 def test_merge():
     '''It will test if the merge works'''
     assert merge({'1':2}, {'1':3}, {}) == {'1':5}
+def test_sets():
     assert merge({'q':set([7, 8, 9])}, {'q':'abcd'}, {}) == {'q': (set([7, 8, 9]), 'abcd')}
+def test_1stfull():
     assert merge({'a':2}, {}, {}) == {'a':2}
+def test_1stempty():
     assert merge({}, {'a':2}, {}) == {'a':2}
 
 
@@ -42,14 +62,16 @@ if __name__ == "__main__":
                   'z': set([1, 2, 3]),
                   'w': 'qweqwe',
                   't': {'a': [1, 2]},
-                  'm': [1]}
+                  'm': [1],
+                  'u': 'oh'
+                  }
     SECOND_DICT = {'x': [4, 5, 6],
                    'y': 4,
                    'z': set([4, 2, 3]),
-                   'n': 'abc',
                    'w': 'asdf',
                    't': {'a': [3, 2]},
-                   'm': "wer"
+                   'm': "wer",
+                   'n': 'abc'
                    }
     NEW_DICT = {}
     MERGED = (merge(FIRST_DICT, SECOND_DICT, NEW_DICT))
