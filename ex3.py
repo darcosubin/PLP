e39ca52cb07b9a0c2a5eb6880df94051b8857c42
Darco Subin <darco.subin@3pillar.corp@tim-lap-085.3pillar.corp>
2019-08-20 14:34:55 +0300

    ex3.py

--- ex3.py
+++ ex3.py
@@ -7,17 +7,26 @@ def readfromfile():
         entry=iff.readlines()
     return entry
 
-
+def conc(list_a, list_b):
+    """This will concatenate the two given lists"""
+    flattened = []
+    for element_i in [list_a, list_b]:
+        for element_in_i in element_i:
+            flattened.append(element_in_i)
+    return flattened
 
 def splitndict():
     variablesRead=readfromfile()
     splitted=[i.split() for i in variablesRead]
-    for j in splitted:
-        d={}
-        d[j[0]]=int(j[1])
-        print d
-    print splitted
-    return splitted
+    d={}
+    for lines in splitted:
+        if lines=="\n":
+            splines=lines.split()
+        for line in splines:
+            line=conc(lines[0],lines[1])
+            d[line[0]]=int(line[1])
+            print d
+    return d
 
 
 def writeinfile():
