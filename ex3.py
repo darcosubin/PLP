a3f2622cbb2b596f635a9a10187561cc14ee9765
Darco Subin <darco.subin@3pillar.corp@tim-lap-085.3pillar.corp>
2019-08-21 14:53:37 +0300

    ex3.py

--- ex3.py
+++ ex3.py
@@ -1,32 +1,32 @@
-entry=" "
-str1=" "
-str2=0
-x=0
 def readfromfile():
     with open ("inf.txt", 'r') as iff:
         entry=iff.readlines()
     return entry
 
-def conc(list_a, list_b):
-    """This will concatenate the two given lists"""
-    flattened = []
-    for element_i in [list_a, list_b]:
-        for element_in_i in element_i:
-            flattened.append(element_in_i)
-    return flattened
 
 def splitndict():
+    listofd=[]
     variablesRead=readfromfile()
     splitted=[i.split() for i in variablesRead]
     d={}
     for lines in splitted:
-        if lines=="\n":
-            splines=lines.split()
-        for line in splines:
-            line=conc(lines[0],lines[1])
-            d[line[0]]=int(line[1])
-            print d
-    return d
+        if lines:
+            d[lines[0]]=int(lines[1])
+        elif d=={}:
+            pass
+        else:
+            listofd.append(d)
+            d={}
+        print listofd
+    return listofd
+
+
+def sortdicts():
+    listofs=[]
+    listofs=splitndict()
+    for item in listofs:
+        item=sorted(listofs, key=lambda d: min(d.keys()))
+        print item
 
 
 def writeinfile():
@@ -35,6 +35,6 @@ def writeinfile():
 
 
 if __name__ == "__main__":
-    readfromfile()
+
     writeinfile()
-    splitndict()
+    sortdicts()
