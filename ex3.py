ec6594ad54c5d3e867b4d540b24c35e3094fb2a2
Darco Subin <darco.subin@3pillar.corp@tim-lap-085.3pillar.corp>
2019-08-22 14:30:32 +0300

    ex3.py

--- ex3.py
+++ ex3.py
@@ -4,8 +4,18 @@ def readfromfile():
     return entry
 
 
-def splitndict():
-    listofd=[]
+def enum(listofj=[]):
+    listofj=[]
+    listen=list(enumerate(splitndict(), start=1))
+    for index in listen:
+        for jndex in index:
+            listofj.append(jndex)
+            break
+    print listofj
+    return listofj
+
+
+def splitndict(listofd=[]):
     variablesRead=readfromfile()
     splitted=[i.split() for i in variablesRead]
     d={}
@@ -22,16 +32,22 @@ def splitndict():
 
 
 def sortdicts():
+    listofd=[]
     listofs=[]
-    listofs=splitndict()
-    for item in listofs:
-        item=sorted(listofs, key=lambda d: min(d.keys()))
-        print item
+    listofs=splitndict(listofd)
+    sorteds=sorted(listofs, key=lambda items: min(items.values()))
+    print sorteds
+    return sorteds
+
+
 
 
 def writeinfile():
+    x=str(enum(sortdicts()))
     with open ("ouf.txt", 'w') as off:
-        off.write(str(splitndict()))
+        off.write(str(sortdicts()))
+        off.write("\n\n")
+        off.write(x)
 
 
 if __name__ == "__main__":
