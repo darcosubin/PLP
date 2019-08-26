35d85a0f39a8d273420c1771e9930be7738308da
Darco Subin <darco.subin@3pillar.corp@tim-lap-085.3pillar.corp>
2019-08-26 11:34:48 +0300

    ex3.py

--- ex3.py
+++ ex3.py
@@ -1,12 +1,17 @@
+'''Sorting two dictionaries by their keys'''
+
+
 def readfromfile():
-    with open ("inf.txt", 'r') as iff:
-        entry=iff.readlines()
+    '''Reading from file'''
+    with open("inf.txt", 'r') as iff:
+        entry = iff.readlines()
     return entry
 
 
-def enum(listofj=[]):
-    listofj=[]
-    listen=list(enumerate(splitndict(), start=1))
+def enum(listofj):
+    '''Enumerating'''
+    listofj = []
+    listen = list(enumerate(splitndict(), start=1))
     for index in listen:
         for jndex in index:
             listofj.append(jndex)
@@ -15,39 +20,56 @@ def enum(listofj=[]):
     return listofj
 
 
-def splitndict(listofd=[]):
-    variablesRead=readfromfile()
-    splitted=[i.split() for i in variablesRead]
-    d={}
+def splitndict():
+    '''Splitting and making a dict'''
+    listofd = []
+    variablesread = readfromfile()
+    splitted = [i.split() for i in variablesread]
+    newdict = {}
     for lines in splitted:
         if lines:
-            d[lines[0]]=int(lines[1])
-        elif d=={}:
+            newdict[lines[0]] = int(lines[1])
+        elif newdict == {}:
             pass
         else:
-            listofd.append(d)
-            d={}
-        print listofd
-    return listofd
+            listofd.append(newdict)
+            newdict = {}
+        listen = list(enumerate(listofd, start=1))
+        print listen
+    return listen
 
 
 def sortdicts():
-    listofd=[]
-    listofs=[]
-    listofs=splitndict(listofd)
-    sorteds=sorted(listofs, key=lambda items: min(items.values()))
-    print sorteds
+    '''Sorting the dicts'''
+    listofs = []
+    listofs = splitndict()
+    sorteds = sorted(listofs, key=lambda items: min(items[1].values()))
+    print ([
+        i[0]
+        for i in sorteds
+    ])
     return sorteds
 
 
-
-
 def writeinfile():
-    x=str(enum(sortdicts()))
-    with open ("ouf.txt", 'w') as off:
-        off.write(str(sortdicts()))
+    '''Writing in file'''
+    sortedd = sortdicts()
+    ind = [
+        iterator[0]
+        for iterator in sortedd
+    ]
+    inx = [
+        jterator[1]
+        for jterator in sortedd
+    ]
+    with open("ouf.txt", 'w') as off:
+        off.write("Sorted dictionaries by vaule from keys are: ")
+        off.write("\n")
+        off.write(str(inx))
         off.write("\n\n")
-        off.write(x)
+        off.write("Sorted index of dictionaries are: ")
+        off.write("\n")
+        off.write(str(ind))
 
 
 if __name__ == "__main__":
